ac0cbfa70256544ad9c7c76721c81f2c6b5f852e
statjhw <136469229+statjhw@users.noreply.github.com>
2025-04-07 20:36:38 +0900

    fix: driver bug 수정

--- job_integration/driver.py
+++ job_integration/driver.py
@@ -3,10 +3,8 @@ from selenium.webdriver.chrome.options import Options
 
 
 def get_chrome_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    return webdriver.Chrome(options=chrome_options)
+    try:
+        driver = webdriver.Chrome()
+        return driver
+    except: 
+        print("driver 생성 에러")
